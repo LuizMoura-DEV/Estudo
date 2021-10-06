84f4b75809c0f11a00f790255990d9601b365381
Luiz Moura <luizmoura.f1@gmail.com>
2021-10-06 00:07:04 -0300

    Finalizando estudo

--- JAVA/exercicios/src/fundamentos/Inferencia.java
+++ JAVA/exercicios/src/fundamentos/Inferencia.java
@@ -5,19 +5,19 @@ public class Inferencia {
 	public static void main(String[] args) {
 		double a = 4.5;
 		System.out.println(a);
-		
+
 		var b = 4.5;
 		System.out.println(b);
-		
+
 		var c = "Texto";
 		System.out.println(c);
-		
+
 		c = "Outro texto";
 		System.out.println(c);
-		
+
 		double d;		// váriavel foi declarada
 		d = 123.65;		// váriavel foi iniciaçozada
 		System.out.println(d);		// váriavel foi usada!
-				
+
 	}
 }
--- JAVA/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ JAVA/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,12 +1,12 @@
 package fundamentos;
 /**
- * 
- * @author Luiz Moura 
+ *
+ * @author Luiz Moura
  * @since JDK 1.0
  */
 public class PrimeiroPrograma {
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		// Sentença de código termina com ;
 		// Mais uma linha...
 		// FIM
