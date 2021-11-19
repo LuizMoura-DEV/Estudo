bb9d36c9fb881e262ab09a4be5d399f8ed1ea87d
Luiz Moura <luizmoura.f1@gmail.com>
2021-11-18 23:06:56 -0300

    2021-11-18

---
+++ Cypress/cypress/integration/arrow.spec.js
@@ -0,0 +1,29 @@
+it('Nada agora', function(){})
+
+//function soma(a,b){
+//    return a + b;
+//}
+
+//const soma = function (a,b){
+//    return a + b
+//}
+
+//const soma = (a,b)=>{
+//    return a + b
+//}
+
+//const soma = (a,b) => a + b
+
+//const soma = a => a+a 
+
+const soma = () => 5 + 5
+
+console.log(soma(1,2))
+
+it('a function test...', function(){
+    console.log('Function', this)
+})
+
+it('an arrow test...', () => {
+    console.log('Arrow', this)
+})
---
+++ Cypress/cypress/integration/describe.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+it.only('A externa test...', ()=>{
+
+})
+
+describe('Should group tests...',() =>{
+
+    describe('Should group more specific tests...', () =>{
+        
+        it('A specific test 1...', ()=>{
+    
+        })
+        
+        it('A specific test 2...', ()=>{
+    
+        })
+
+
+    })
+        
+    it('A internal test...', ()=>{
+
+    })
+
+})
